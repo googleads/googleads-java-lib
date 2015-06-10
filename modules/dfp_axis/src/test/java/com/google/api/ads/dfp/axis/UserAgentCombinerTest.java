bcc26f64222de57b5916b7ef0af87ee41972ea84
Josh Radcliff <jradcliff@google.com>
2015-06-10 13:40:56 -0400

    Preparing release 2.1.0

--- modules/dfp_axis/src/test/java/com/google/api/ads/dfp/axis/UserAgentCombinerTest.java
+++ modules/dfp_axis/src/test/java/com/google/api/ads/dfp/axis/UserAgentCombinerTest.java
@@ -33,7 +33,7 @@ import org.junit.runners.JUnit4;
 public class UserAgentCombinerTest extends TestCase {
 
   private static final String USER_AGENT =
-      "userAgent (DfpApi-Java, Dfp-Axis/2.0.0, Common-Java/2.0.0, Axis/1.4, Java/"
+      "userAgent (DfpApi-Java, Dfp-Axis/2.1.0, Common-Java/2.1.0, Axis/1.4, Java/"
           + System.getProperty("java.version") + ", maven)";
 
   /**
