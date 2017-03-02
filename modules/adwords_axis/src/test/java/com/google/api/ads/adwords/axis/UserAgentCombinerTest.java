5b44c920a63d15567b8b4c57eb4a8a6c7e6f70bf
Josh Radcliff <jradcliff@users.noreply.github.com>
2017-03-02 11:47:58 -0500

    Preparing release 3.1.1

--- modules/adwords_axis/src/test/java/com/google/api/ads/adwords/axis/UserAgentCombinerTest.java
+++ modules/adwords_axis/src/test/java/com/google/api/ads/adwords/axis/UserAgentCombinerTest.java
@@ -31,7 +31,7 @@ import org.junit.runners.JUnit4;
 public class UserAgentCombinerTest {
 
   private static final String USER_AGENT =
-      "userAgent (AwApi-Java, AdWords-Axis/3.1.0, Common-Java/3.1.0, Axis/1.4, Java/"
+      "userAgent (AwApi-Java, AdWords-Axis/3.1.1, Common-Java/3.1.1, Axis/1.4, Java/"
           + System.getProperty("java.version") + ", maven)";
 
   /**
